01159a4595b1039fe6146b1244bcfbad5ea54709
shobha <2017hw86555@wilp.bits-pilani.ac.in>
2022-06-20 00:46:46 +0530

    Feature2 added

--- helpdeskclient.java
+++ helpdeskclient.java
@@ -1,1 +1,1 @@
-welcome to help desk client - Feature 1
+welcome to help desk client - Feature 1 & Feature 2
--- helpdeskserver.java
+++ helpdeskserver.java
@@ -1,1 +1,1 @@
-welcome to help desk server - Feature 1
+welcome to help desk server - Feature 1 & Feature 2
